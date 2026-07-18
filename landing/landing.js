69fe181b2008fff533f8b53be4f0b3d7737b3093
whostacking <lostfromthelabyrinth@gmail.com>
2026-07-18 18:59:00 +0800

    feat: new landing page

---
+++ landing/landing.js
@@ -0,0 +1,26 @@
+let mouseX = 0;
+let mouseY = 0;
+
+let lerpedX = 0;
+let lerpedY = 0;
+
+document.addEventListener("mousemove", e => {
+    mouseX = e.clientX / window.innerWidth * 20 - 10;
+    mouseY = e.clientY / window.innerHeight * 20 - 10;
+});
+
+const tweets = document.querySelectorAll(".whosnipping");
+
+function update() {
+    lerpedX += (mouseX - lerpedX) * 0.05;
+    lerpedY += (mouseY - lerpedY) * 0.05;
+
+    tweets.forEach(tweet => {
+        tweet.style.transform =
+            `translate(${lerpedX}px, ${lerpedY}px) rotate(${lerpedX * 0.15}deg)`;
+    });
+
+    requestAnimationFrame(update);
+}
+
+update();
